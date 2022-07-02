a369bf1e234bd69622e1c75af554b8b3a7774c20
alvin <applewennie895@163.com>
2022-07-02 19:27:34 +0800

    下载页面

--- src/views/DownloadApp.vue
+++ src/views/DownloadApp.vue
@@ -1,109 +1,155 @@
 <template>
-<div class="box bg-no-repeat flex w-100 flex-end align-center">
-    <div class="info flex flex-column">
-        <span class="app-name font-regular">海牛直播</span>
-        <span class="sub-title font-medium">专业体育比赛直播</span>
-        <ul class="w-100 flex description justify-between align-center font-regular">
-            <li>高颜值主播</li>
-            <li>海量比赛数据</li>
-            <li>大咖相聚聊天</li>
-        </ul>
-        <div class="flex justify-between app-download">
-            <ul class="apps flex flex-column text-center text-white">
-                <li class="ios ">
-                   <svg-icon class="icon-20" icon-class="ios"></svg-icon>
-                    iOS
-                </li>
-                <li class="android">
-                    <svg-icon class="icon-20" icon-class="android"></svg-icon>
-                    Android
-                </li>
-            </ul>
-            <div>
-                <div class="qrcode bg-white">
-                    <div class="code w-100 h-100 bg-gray2">
-                        <qrcode />
+<div class="download-page max-screen">
+    <div class="w-100 outer-wrap bg-no-repeat">
+        <div class="upper-div m-auto  w-100 max-screen">
+            <div class="left-info wrap-1450">
+                <div class="logo-text bg-no-repeat  bg-size-auto m-b-20"></div>
+                <div class="font-regular m-b-20 font-400 d-inline-block site  font-48">haibao18.cn</div>
+                <p class="font-regular font-45 font-400 m-b-30">专业体育比赛直播·精彩触手可及</p>
+                <div class="flex features align-center">
+                    <span>高颜值主播</span>
+                    <span>海量比赛数据</span>
+                    <span>大咖相聚聊天</span>
+                </div>
+                <div class="app-info flex align-center">
+                    <div class="buttons text-white">
+                        <div class="item m-b-20 border-radius-15 flex justify-center align-center w-100">
+                            <icon-png :width="21" :height="22" icon="icons/apple" />
+                            <span class="font-30 m-l-10">ios</span>
+                        </div>
+                        <div class="item m-b-20 border-radius-15 flex justify-center align-center w-100">
+                            <icon-png :width="21" :height="22" icon="icons/android" />
+                            <span class="font-30 m-l-10">Android</span>
+                        </div>
+                    </div>
+                    <div class="code">
+                        <div class="code-box">
+                            <Qrcode />
+                        </div>
+                        <span class="font-20">扫码二维码下载</span>
                     </div>
                 </div>
-                <span class="download-label w-100 d-inline-block text-center">扫描二维码下载</span>
+            </div>
+        </div>
+        <div class="wrap-1450 down-section">
+            <div class="flex justify-between align-center">
+                <div
+                    v-for="item in list"
+                    :key="item.icon"
+                    class="item flex flex-column align-center">
+                <span class="icon bg-center bg-no-repeat bg-size-100" :style="{
+                    backgroundImage: 'url(' + require(`../assets/images/download/${item.icon}.png`) + ')'
+                }"></span>
+                    <div class="w-100 flex flex-column align-center">
+                        <span class="font-25 font-medium description font-500">{{ item.text}}</span>
+                        <span class="font-25 font-medium description font-500">{{ item.text1}}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="right-pic"></div>
 </div>
 </template>
-
 <script>
+import IconPng from '@/components/IconPng'
 import Qrcode from '@/components/Qrcode'
 export default {
     name: 'DownloadApp',
     components: {
+        IconPng,
         Qrcode
+    },
+    data () {
+        return {
+            list: [
+                {
+                    icon: 'ad',
+                    text: '全球赛事',
+                    text1: '高清无广告'
+                },
+                {
+                    icon: 'voice',
+                    text: '专业主播',
+                    text1: '相约看比赛'
+                },
+                {
+                    icon: 'statics',
+                    text: '海量直播数据',
+                    text1: '看直播有依据'
+                },
+                {
+                    icon: 'chat',
+                    text: '互动探讨',
+                    text1: '相聚聊天室'
+                }
+            ]
+        }
     }
 }
 </script>
-
 <style lang="scss" scoped>
-.box {
-    background: linear-gradient(180deg, #FFFFFF 0%, #E3E9FD 100%);
-    //height: 39.1vw;
-    height: calc(100vh - 90px);
-}
-.right-pic {
-    width: 56.7vw;
-    height: 39.1vw;
-    background-image: url('../assets/images/download/pic.png');
-    background-size: 100% 100%;
-    background-position: center;
-}
-.info {
-    width: 24.5vw;
-    .app-name {
-        font-size: 2.9vw;
-        line-height: 4.1vw;
-        color: #627FF5;
+.download-page {
+    background: linear-gradient(0deg, #E6F3FF 0%, #FFFFFF 100%);
+    padding-bottom: 125px;
+    .outer-wrap{
+        background-image: url('../assets/images/download/phone.png');
+        background-size: contain;
+        background-position: left -150px;
     }
-    .sub-title {
-        font-size: 2.9vw;
-        line-height: 4.1vw;
-        color: #232D42;
-        margin-top: 0.42vw;
-    }
-    .description {
-        margin-top: 0.96vw;
-        color: #919CB1;
-        font-size: 0.625vw;
-    }
-    .app-download {
-        margin-top: 3.3vw;
-        .download-label {
-            font-size: 0.83vw;
-            color: #6C778B;
-            margin-top: 0.625vw;
+    .upper-div {
+        padding-top: 280px;
+        color: #0E3F6A;
+        .logo-text{
+            width: 290px;
+            height: 80px;
+            background-position: left bottom;
+            background-size: contain;
+            background-image: url('../assets/images/download/logo-text.png');
         }
-    }
-    .apps {
-        width: 13.5vw;
-        li{
-            height: 4.1vw;
-            line-height: 4.1vw;
-            background: #232D42;
-            border-radius: 8px;
-            margin-bottom: 1.46vw;
-            font-size: 1.25vw;
-        }
-    }
-    .qrcode {
-        //padding: 0.625vw;
-        width: 9.5vw;
-        height: 9.5vw;
-    }
-}
+        .site {
+            line-height: 48px;
 
-::v-deep {
-    .apps {
-        .svg-icon {
-            vertical-align: sub;
+        }
+        .features {
+            span{
+                margin-right: 56px;
+                font-size: 25px;
+                color: #6D9FCB;
+            }
+        }
+        .app-info {
+            margin-top: 60px;
+            .buttons {
+                width: 225px;
+            }
+            .item {
+                background-color: #0E3F6A;
+                line-height: 67px;
+            }
+        }
+        .code {
+            margin-left: 55px;
+            span{
+                color: #6D9FCB;
+            }
+        }
+        .code-box {
+            width: 150px;
+            height: 150px;
+        }
+    }
+    .down-section {
+        margin-top: 400px;
+        .item {
+            .icon {
+                width: 150px;
+                height: 150px;
+                margin-bottom: 35px;
+            }
+            .description {
+                color: #182648;
+                line-height: 27px;
+            }
         }
     }
 }
