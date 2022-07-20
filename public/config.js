6b731a180fca61420551ffbf5ac188e4f63d54b1
alvin <applewennie895@163.com>
2022-07-20 23:14:49 +0800

    修改跳转环境

--- public/config.js
+++ public/config.js
@@ -1,12 +1,13 @@
 
 /*eslint-disable */
 /*测试环境*/
+const mUrl = 'http://h5.wuhaicj.com/'
 const _appDownloadUlr = 'http://zhuoyixiaochengxu.cn/haibao_100.apk'
 const _requestApiUrl = 'http://api.wuhaicj.com'
 const _requestWS = 'ws://ws.wuhaicj.com/'
 
 /*线上环境*/
-const mUrl = 'https://m.haibao18.cn/'
+// const mUrl = 'https://m.haibao18.cn/'
 // const _appDownloadUlr = 'https://cri1jba.cn/haibao_100.apk'
 // const _requestApiUrl = 'https://api.hljhwyy.cn'
 // const _requestWS = 'wss://ws.qiangjinmkd.cn'
